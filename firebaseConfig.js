b5e1d71c08dde56e8dae97541771842b48ffc185
Himesh <himesh@gmail.com>
2025-03-04 18:35:31 +0530

    Firebase change

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,13 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase
 import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js"; 
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyD9cJFhNU2HeHxA5TzBccG59co1MJfqIMI",
+    authDomain: "ikarus-3d-9f3c9.firebaseapp.com",
+    projectId: "ikarus-3d-9f3c9",
+    storageBucket: "ikarus-3d-9f3c9.firebasestorage.app",
+    messagingSenderId: "392626052388",
+    appId: "1:392626052388:web:a128e39558ed509aaae7b1",
+    measurementId: "G-LPMNKK9Q6S"
+  };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
